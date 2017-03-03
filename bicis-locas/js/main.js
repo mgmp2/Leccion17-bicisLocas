400b95ed84868ea54e3a8b462a740ce6e3841043
Miriam Mendoza <miriammp1997@gmail.com>
2017-03-03 17:35:54 -0500

    Validaciones listas

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -1,24 +1,49 @@
 function validateForm(){
 
-  var nom   = document.getElementById("name");
-  var ap    = document.getElementById("lastname");
-  var email = document.getElementById("input-email");
-  var pas   = document.getElementById("input-password");
+    var nom   = document.getElementById("name");
+    var ap    = document.getElementById("lastname");
+    var email = document.getElementById("input-email");
+    var pas   = document.getElementById("input-password");
+    var op    = document.getElementById("opciones");
 
+    var expRegName  = /^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/g;
+    var expRegLast  = /[^a-z-123456789]+([a-z]{2,12})+\s([^a-z-123456789]+([a-z]{2,8}))/g;
+    var expRegEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    var expRegPass  =  /^[a-zA-z]{6,}$/;
 
-  var expRegName  = /^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/g;
-  var expRegLast  = /[^a-z-123456789]+([a-z]{2,12})+\s([^a-z-123456789]+([a-z]{2,8}))/g;
-  var expRegEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-  if(!expRegName.test(nom.value)){
-    alert("Escribir correctamente \nCampo nombre: empezando por Mayúscula");
-  }
+    //Name
+    if(!expRegName.test(nom.value)){
+      alert("Escribir correctamente \nCampo nombre: empezando por Mayúscula");
+      nom.focus();
+    }
 
-  else if(!expRegLast.test(ap.value)){
-    alert("Escribir correctamente \nCampo apellido: Sus dos apellidos empezando por Maýucula");
-  }
-  else if(!expRegEmail.test(email.value)){
+    //Last Name
+    else if(!expRegLast.test(ap.value)){
+      alert("Escribir correctamente \nCampo apellido: Sus dos apellidos empezando por Maýucula");
+      ap.focus();
+    }
 
-    alert("Escribir correctament \nCampo email de manera correcta");
-  }
+    //Email
+    else if(!expRegEmail.test(email.value)){
+          alert("Escribir correctamente \nCampo email de manera correcta");
+          email.focus();
+    }
 
+    //Password
+    else if(!expRegPass.test(pas.value)){
+      if(pas.value == "123456" || pas.value == "98754"){
+        alert("Escribir correctamente \nCampos de contraseña diferente de: 123456 o 98754");
+        pas.focus();
+      }
+      else if(pas.value == ""){
+        alert("Debe llenar el campo de contraseña!!");
+        pas.focus();
+      }
+    }
+
+    //options
+    else if(op.value==0){
+      alert("Seleccion alguna opción en su tipo de bicicleta");
+      op.focus();
+    }
 }
